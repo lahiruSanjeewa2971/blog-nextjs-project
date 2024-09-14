2cb5017109746203d85095d752d47be93b1d82e4
lahiruSanjeewa2971 <rathne1997@gmail.com>
2024-09-14 18:36:49 +0530

    test 2

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -3,6 +3,11 @@ import Post from '@/app/ui/components/posts/Post';
 
 export default function Page({ params }: { params: { id: string } }) {
   const post = posts.find((post) => post.id === params.id);
+
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
   return (
     <>
       <h1>Post</h1>
