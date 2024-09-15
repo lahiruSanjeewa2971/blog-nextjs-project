7465d0ee40c1af6bc34e4d8e19ac4a57a8e3ad8a
lahiruSanjeewa2971 <rathne1997@gmail.com>
2024-09-16 02:17:03 +0530

    finish project

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,6 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
+import { auth } from '../../../../auth.config';
 
 export async function GET() {
     try {
@@ -12,15 +13,18 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
+    const session = await auth();
     const { searchParams } = new URL(request.url);
     const id = searchParams.get('id');
     const title = searchParams.get('title');
     const content = searchParams.get('content');
     const date = searchParams.get('date');
+    const author = searchParams.get('author');
 
     try {
+        if (!session) { return NextResponse.json({ message: 'Unauthorized access.' }, { status: 401 }); }
         // SQL query to insert a new post
-        await sql`INSERT INTO posts (id, author, title, content, date) VALUES (${id}, 'sandra l', ${title}, ${content}, ${date});`;
+        await sql`INSERT INTO posts (id, author, title, content, date) VALUES (${id}, ${author}, ${title}, ${content}, ${date});`;
         return NextResponse.json({ message: 'Post successfully inserted' }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,18 +1,23 @@
-// import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
 import { connectToDB, getPosts } from '@/app/lib/data';
 import Link from 'next/link';
 import { Button } from '@/app/ui/components/button';
+import { auth } from '../../../../auth.config';
 
 export default async function Page() {
   const client = await connectToDB();
   const posts = await getPosts();
+  const session = await auth();
 
   return (
     <>
       {client && <p className='text-green-500'>Connected to DB</p>}
 
-      <Link href="/blog/post/insert"><Button className="outline outline-1  border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">New +</Button></Link>
+      {session?.user && <Link href="/blog/post/insert">
+        <Button className="outline outline-1  border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">
+          New +
+        </Button>
+      </Link>}
 
       <h1>Posts</h1>
       {posts?.map((post) => <Post id={''} title={''} content={''} date={''} key={post.id} {...post} />)}
